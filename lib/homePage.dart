79760c06bb80a40daf0a6e51b742ab78bc609d05
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-08-17 06:43:01 +0300

    Created the homePage.dart file. Added the flutter_svg package as a dependency. Added the assets folder which contains the icons in SVG format. Modified the pubspec.yaml file to support the dependencies main.dart file remains untouched VI Note: Make sure to press the get Dependencies button when a notification indicating that changes have happened to the pubspec.yaml file appears at the top of the editor, if it does.

---
+++ lib/homePage.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 10,
+        child: Scaffold(
+          appBar: flexploreAppBar(),
+          backgroundColor: const Color(0xfff6f1f1),
+        )
+    );
+  }
+
+  AppBar flexploreAppBar(){
+    return AppBar(
+      title: const Text("Flexplore Logo",
+        style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+      bottom: const PreferredSize(
+        preferredSize: Size(0, 50),
+        child: ColoredBox(color: Color(0xffafd3e2),
+          child: TabBar(tabs: [
+            Tab(text: "Recommended",),
+            Tab(text: "History",),
+            Tab(text: "Explore",)
+          ],
+          ),
+        ),
+
+      ),
+      elevation: 0,
+      centerTitle: true,
+      backgroundColor: const Color(0xff146c94),
+      leading: GestureDetector(
+        onTap: (){
+          //TODO WHEN TAPPED CODE GOES HERE
+        },
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10)
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/drawer_icon.svg',
+            height: 20,
+            width: 20,
+          ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: (){
+            //TODO WHEN TAPPED CODE GOES HERE
+          },
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10)
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/profile_icon.svg',
+              height: 50,
+              width: 50,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
