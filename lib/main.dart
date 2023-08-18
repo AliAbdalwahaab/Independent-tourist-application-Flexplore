98010d2f25a0ef19f4ffd271628f93584599f051
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-08-18 05:40:50 +0300

    Removed DebugShowCheckedModeBanner by setting it to false in the main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:indeoendent_tourist_app_main/recommended_page.dart';
 
 void main() {
   runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       body: HomePage(),
     ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:indeoendent_tourist_app_main/main.dart';
+import 'package:indeoendent_tourist_app_main/side_menu.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
