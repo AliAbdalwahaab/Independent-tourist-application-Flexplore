5a37bcc6b3ba8d5684ee6e8c77acb6fbd57adff7
AHMED HAMED <ahmed_hamed2242@yahoo.com>
2023-08-20 22:44:37 +0300

    changed tabBar length to 3 instead of 10

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  DefaultTabController(
-        length: 10,
+        length: 3,
         child: Scaffold(
           appBar: flexploreAppBar(),
           backgroundColor:  Color(0xfff6f1f1),
