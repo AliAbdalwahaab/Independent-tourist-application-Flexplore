cbf1c790a73de51d2035c196aec422da47a49bac
BolesWaheed <boleswaheed477@gmail.com>
2023-09-02 19:14:35 +0300

    add feedback_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:indeoendent_tourist_app_main/DuringTrip.dart';
+import 'package:indeoendent_tourist_app_main/feedback/feedback_page.dart';
 import 'package:indeoendent_tourist_app_main/homePage.dart';
 import 'package:indeoendent_tourist_app_main/recommended_page.dart';
 import 'package:indeoendent_tourist_app_main/trip_info.dart';
@@ -10,5 +12,7 @@ void main() {
   //     body: HomePage(),
   //   ),
   // ));
-  runApp(const TripInfo());
+  runApp(const MaterialApp(
+    home: FeedBack(),
+  ));
 }
